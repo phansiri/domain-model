dfa150c23857f99f4742b721725f6d207a16bb6c
lit_phansiri <phansiri@uw.edu>
2016-10-15 16:40:52 -0700

    test works for Money, Jobs, and Persons

--- SimpleDomainModel/main.swift
+++ SimpleDomainModel/main.swift
@@ -22,20 +22,37 @@ open class TestMe {
 // Money
 //
 public struct Money {
-    public var amount : Int // pennies
+    public var amount : Int
     public var currency : String
     
-    enum Currency {
+    enum Currency: String {
         case USD, EUR, CAN, GBP
     }
     
+    private let conversionRate = [
+        "usdToGBP": 0.5,
+        "usdToEUR": 1.5,
+        "usdToCAN": 1.25,
+        "canToGBP": 0.61,
+        "canToEUR": 0.6667,
+        "canToUSD": 0.8,
+        "gbpToUSD": 2.0,
+        "gbpToEUR": 1.11,
+        "gbpToCAN": 1.64,
+        "eurToGBP": 0.90,
+        "eurToUSD": 0.6667,
+        "eurToCAN": 1.48,
+        ]
+    
     init(amount: Int, currency: String) {
-        self.amount = amount
         self.currency = currency
+        self.amount = amount
+
     }
 
     
     public func convert(_ to: String) -> Money {
+        
 //        switch value {
 //        case .GBP:
 //            switch
@@ -61,58 +78,59 @@ public struct Money {
         case "GBP":
             switch to {
             case "USD":
-                return Money(amount: Int(Double(self.amount) * conversionRate["gbpToUSD"]!) * 100, currency: "USD")
+                return Money(amount: Int(Double(self.amount) * conversionRate["gbpToUSD"]!), currency: "USD")
             case "EUR":
-                return Money(amount: Int(Double(self.amount) * conversionRate["gbpToEUR"]!) * 100, currency: "EUR")
+                return Money(amount: Int(Double(self.amount) * conversionRate["gbpToEUR"]!), currency: "EUR")
             case "CAN":
-                return Money(amount: Int(Double(self.amount) * conversionRate["gbpToCAN"]!) * 100, currency: "CAN")
+                return Money(amount: Int(Double(self.amount) * conversionRate["gbpToCAN"]!), currency: "CAN")
             default:
                 return Money(amount: self.amount, currency: self.currency)
             }
         case "CAN":
             switch to {
             case "USD":
-                return Money(amount: Int(Double(self.amount) * conversionRate["canToUSD"]!) * 100, currency: "USD")
+                return Money(amount: Int(Double(self.amount) * conversionRate["canToUSD"]!), currency: "USD")
             case "EUR":
-                return Money(amount: Int(Double(self.amount) * conversionRate["canToEUR"]!) * 100, currency: "EUR")
+                return Money(amount: Int(Double(self.amount) * conversionRate["canToEUR"]!), currency: "EUR")
             case "GBP":
-                return Money(amount: Int(Double(self.amount) * conversionRate["canToGBP"]!) * 100, currency: "GBP")
+                return Money(amount: Int(Double(self.amount) * conversionRate["canToGBP"]!), currency: "GBP")
             default:
                 return Money(amount: self.amount, currency: self.currency)
             }
         case "EUR":
             switch to {
             case "USD":
-                return Money(amount: Int(Double(self.amount) * conversionRate["eurToUSD"]!) * 100, currency: "USD")
+                return Money(amount: Int(Double(self.amount) * conversionRate["eurToUSD"]!), currency: "USD")
             case "EUR":
-                return Money(amount: Int(Double(self.amount) * conversionRate["eurToGBP"]!) * 100, currency: "GBP")
+                return Money(amount: Int(Double(self.amount) * conversionRate["eurToGBP"]!), currency: "GBP")
             case "CAN":
-                return Money(amount: Int(Double(self.amount) * conversionRate["eurToCAN"]!) * 100, currency: "CAN")
+                return Money(amount: Int(Double(self.amount) * conversionRate["eurToCAN"]!), currency: "CAN")
             default:
                 return Money(amount: self.amount, currency: self.currency)
             }
         default:
             switch to {
-            case "USD":
-                return Money(amount: Int(Double(self.amount) * conversionRate["usdToGBP"]!) * 100, currency: "GBP")
+            case "GBP":
+                return Money(amount: Int(Double(self.amount) * conversionRate["usdToGBP"]!), currency: "GBP")
             case "EUR":
-                return Money(amount: Int(Double(self.amount) * conversionRate["usdToEUR"]!) * 100, currency: "EUR")
+                return Money(amount: Int(Double(self.amount) * conversionRate["usdToEUR"]!), currency: "EUR")
             case "CAN":
-                return Money(amount: Int(Double(self.amount) * conversionRate["usdToCAN"]!) * 100, currency: "CAN")
+                return Money(amount: Int(Double(self.amount) * conversionRate["usdToCAN"]!), currency: "CAN")
             default:
-                return Money(amount: self.amount * 100, currency: self.currency)
+                return Money(amount: self.amount, currency: self.currency)
             }
         }
     }
-}
 
-//    public func add(_ to: Money) -> Money {
-//        return Money(amount: to.amount + self.convert(to.currency).amount, currency: to.currency)
-//    }
-//    
-//    public func subtract(_ from: Money) -> Money {
-//        return Money(amount: from.amount + self.convert(from.currency).amount, currency: from.currency)
-//    }
+
+    public func add(_ to: Money) -> Money {
+        return Money(amount: to.amount + self.convert(to.currency).amount, currency: to.currency)
+    }
+    
+    public func subtract(_ from: Money) -> Money {
+        return Money(amount: from.amount + self.convert(from.currency).amount, currency: from.currency)
+    }
+}
 
 
 let tenUSD = Money(amount: 10, currency: "USD")
@@ -121,95 +139,119 @@ let fiveGBP = Money(amount: 5, currency: "GBP")
 let fifteenEUR = Money(amount: 15, currency: "EUR")
 let fifteenCAN = Money(amount: 15, currency: "CAN")
 
-//let total = tenUSD.add(tenUSD)
-//print(total.amount == 20)
-//print(total.currency == "USD")
+let gbp = tenUSD.convert("GBP")
+print("tenUSD: \(tenUSD.amount)")
+print("gbp: \(gbp.amount)")
+print(gbp.currency == "GBP")
+print(gbp.amount == 5)
 
 
 ////////////////////////////////////
 // Job
 //
 open class Job {
-  fileprivate var title : String
-  fileprivate var type : JobType
+    fileprivate var title : String
+    fileprivate var type : JobType
 
-  public enum JobType {
-    case Hourly(Double)
-    case Salary(Int)
-  }
-  
-  public init(title : String, type : JobType) {
-    self.title = title
-    self.type = type
-  }
-  
-  open func calculateIncome(_ hours: Int) -> Int {
-    switch self.type {
-    case .Hourly(let time):
-        return Int(time * Double(hours))
-    case .Salary(let time):
-        return time
+
+    public enum JobType {
+        case Hourly(Double)
+        case Salary(Int)
     }
-  }
   
-  open func raise(_ amt : Double) {
-    switch self.type {
-    case .Hourly(let hourly):
-        self.type = Job.JobType.Hourly(hourly + amt)
-    case .Salary(let salary):
-        self.type = Job.JobType.Salary(salary + Int(amt))
+
+    public init(title : String, type : JobType) {
+        self.title = title
+        self.type = type
     }
-  }
+  
+
+    open func calculateIncome(_ hours: Int) -> Int {
+        switch self.type {
+        case .Hourly(let time):
+            return Int(time * Double(hours))
+        case .Salary(let time):
+            return time
+        }
+    }
+  
+    open func raise(_ amt : Double) {
+        switch self.type {
+        case .Hourly(let hourly):
+            self.type = Job.JobType.Hourly(hourly + amt)
+        case .Salary(let salary):
+            self.type = Job.JobType.Salary(salary + Int(amt))
+        }
+    }
 }
 
-
-/*
 ////////////////////////////////////
 // Person
 //
 open class Person {
-  open var firstName : String = ""
-  open var lastName : String = ""
-  open var age : Int = 0
+    open var firstName : String = ""
+    open var lastName : String = ""
+    open var age : Int = 0
 
-  fileprivate var _job : Job? = nil
-  open var job : Job? {
-    get { }
-    set(value) {
+    fileprivate var _job : Job? = nil
+    open var job : Job? {
+        get {
+            return _job
+        }
+        set(value) {
+            if self.age >= 16 {
+                self._job = value
+            }
+        }
     }
-  }
   
-  fileprivate var _spouse : Person? = nil
-  open var spouse : Person? {
-    get { }
-    set(value) {
+    
+    fileprivate var _spouse : Person? = nil
+    open var spouse : Person? {
+        get {
+            return _spouse
+        }
+        set(value) {
+            if self.age >= 18 {
+                self._spouse = value
+            }
+        }
     }
-  }
   
-  public init(firstName : String, lastName: String, age : Int) {
-    self.firstName = firstName
-    self.lastName = lastName
-    self.age = age
-  }
+    public init(firstName : String, lastName: String, age : Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+    }
   
-  open func toString() -> String {
-  }
+    open func toString() -> String {
+        return ("[Person: firstName:\(firstName) lastName:\(lastName) age:\(age) job:\(job) spouse:\(spouse)]")
+    }
 }
 
 ////////////////////////////////////
 // Family
 //
 open class Family {
-  fileprivate var members : [Person] = []
+    fileprivate var members : [Person] = []
   
-  public init(spouse1: Person, spouse2: Person) {
-  }
+    public init(spouse1: Person, spouse2: Person) {
+        spouse1.spouse = spouse2
+        spouse2.spouse = spouse1
+        members.append(spouse1)
+        members.append(spouse2)
+    }
   
-  open func haveChild(_ child: Person) -> Bool {
-  }
-  
-  open func householdIncome() -> Int {
-  }
+    open func haveChild(_ child: Person) -> Bool {
+        child.age = 0
+        members.append(child)
+        return true
+    }
+    
+    open func householdIncome() -> Int {
+        let result: Int = 0
+        return result
+    }
 }
-*/
+
 
